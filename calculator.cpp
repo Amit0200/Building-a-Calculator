7eec9ad58806dd9e77635370cfac1c7738e9a5f2
Amit Gupta <116447781+Amit0200@users.noreply.github.com>
2023-12-25 12:42:29 -0600

    Update calculator.cpp
    Declared some character and double variables, input/output using <iostream>, created bunch of operations statements using switch case statement that gives the desired output.

--- calculator.cpp
+++ calculator.cpp
@@ -1,1 +1,45 @@
 
+#include <iostream>
+
+int main() {
+
+  char operator;
+  double number1;
+  double number2;
+  double result;
+
+  std::cout << "------------------ CALCULATOR --------------------" <<std::endl;
+
+  std::cout << " Select one operation (+ - * /) : ";
+  std::cin << operator;
+
+  std::cout << "Enter number 1: ";
+  std::cin number1;
+
+  std::cout << "Enter number 2: ";
+  std::cin number2;
+
+  switch(operator){
+
+    case '+':
+       result = number1 + number2;
+       std::cout << "result: " << result << std::endl;
+       break;
+    case '-':
+       result = number1 - number2;
+       std::cout << "result: " << result << std::endl;
+       break;
+    case '*':
+       result = number1 * number2;
+       std::cout << "result: " << result << std::endl;
+       break;
+    case '/':
+       result = number1 / number2;
+       std::cout << "result: " << result << std::endl;
+       break;
+    default:
+       std::cout << "Enter a valid operator (+,-,*,/)" << std::endl;
+       break;
+  }
+  return o;
+}
